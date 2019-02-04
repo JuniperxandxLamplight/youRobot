5f0381e85ef9ebebcab8f720c1d4880c2cec031b
Aurora Shido-Wagenet and Nayomi Morita <nayomi@>
2019-02-04 15:28:45 -0800

    add css styling and character statsbar

--- js/front-end.js
+++ js/front-end.js
@@ -1,59 +1,75 @@
 $(function(){
   var playerCharStats = new charStats
+  $("#characterBar h5").click(function(){
+    $("#characterBar p").slideToggle();
+  });
+
   $("#scenario1Form").submit(function(event){
     event.preventDefault();
-    $("#scenario1").hide();
-    $("#scenario1Outcome").show();
+    $("#scenario1").slideUp(500);
+    $("#scenario1Outcome").fadeIn(500).delay(500);
     var stat = $("input:radio[name=scenario1Options]:checked").val();
     playerCharStats.pushStat(stat)
-    console.log(playerCharStats);
+    $("#agilDisplay").html(playerCharStats.agility);
+    $("#strDisplay").html(playerCharStats.strength);
+    $("#charDisplay").html(playerCharStats.charisma);
+    $("#intDisplay").html(playerCharStats.intelligence);
 
 
   });
   $("#scenario1Complete").click(function(){
-    $("#scenario1Outcome").hide();
-    $("#scenario2").show();
+    $("#scenario1Outcome").slideUp(500);
+    $("#scenario2").fadeIn(500).delay(500);
 
   });
   $("#scenario2Form").submit(function(event){
     event.preventDefault();
-    $("#scenario2").hide();
-    $("#scenario2Outcome").show();
+    $("#scenario2").slideUp(500);
+    $("#scenario2Outcome").fadeIn(500).delay(500);
     var stat = $("input:radio[name=scenario2Options]:checked").val();
     playerCharStats.pushStat(stat)
-    console.log(playerCharStats);
+    $("#agilDisplay").html(playerCharStats.agility);
+    $("#strDisplay").html(playerCharStats.strength);
+    $("#charDisplay").html(playerCharStats.charisma);
+    $("#intDisplay").html(playerCharStats.intelligence);;
 
   });
   $("#scenario2Complete").click(function(){
-    $("#scenario2Outcome").hide();
-    $("#scenario3").show();
+    $("#scenario2Outcome").slideUp(500);
+    $("#scenario3").fadeIn(500).delay(500);
 
   });
   $("#scenario3Form").submit(function(event){
     event.preventDefault();
-    $("#scenario3").hide();
-    $("#scenario3Outcome").show();
+    $("#scenario3").slideUp(500);
+    $("#scenario3Outcome").fadeIn(500).delay(500);
     var stat = $("input:radio[name=scenario3Options]:checked").val();
     playerCharStats.pushStat(stat)
-    console.log(playerCharStats);
+    $("#agilDisplay").html(playerCharStats.agility);
+    $("#strDisplay").html(playerCharStats.strength);
+    $("#charDisplay").html(playerCharStats.charisma);
+    $("#intDisplay").html(playerCharStats.intelligence);;
 
   });
   $("#scenario3Complete").click(function(){
-    $("#scenario3Outcome").hide();
-    $("#scenario4").show();
+    $("#scenario3Outcome").slideUp(500);
+    $("#scenario4").fadeIn(500).delay(500);
 
   });
   $("#scenario4Form").submit(function(event){
     event.preventDefault();
-    $("#scenario4").hide();
-    $("#scenario4Outcome").show();
+    $("#scenario4").slideUp(500);
+    $("#scenario4Outcome").fadeIn(500).delay(500);
     var stat = $("input:radio[name=scenario4Options]:checked").val();
     playerCharStats.pushStat(stat)
-    console.log(playerCharStats);
+    $("#agilDisplay").html(playerCharStats.agility);
+    $("#strDisplay").html(playerCharStats.strength);
+    $("#charDisplay").html(playerCharStats.charisma);
+    $("#intDisplay").html(playerCharStats.intelligence);;
 
   });
   $("#scenario4Complete").click(function(){
-    $("#scenario4Outcome").hide();
-    $("#results").show();
+    $("#scenario4Outcome").slideUp(500);
+    $("#results").fadeIn(500).delay(500);
   });
 });
