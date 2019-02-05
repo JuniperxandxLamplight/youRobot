76f27c40c9449bfb34a7278eb866337a27b0f89a
Student <example@example.com>
2019-02-05 08:49:59 -0800

    fix merging bugs between story and styling

--- js/front-end.js
+++ js/front-end.js
@@ -10,27 +10,16 @@ $(function(){
     var stat = $("input:radio[name=scenario1Options]:checked").val();
     playerCharStats.pushStatFirst(stat)
     $("#scenario1Outcome-"+stat).fadeIn(500).delay(500);
-    console.log(playerCharStats);
-
-
-  });
-  $(".scenario1Complete").click(function(){
-    $("#scenario1").slideUp(500);
-    $("#scenario1Outcome").fadeIn(500).delay(500);
-    var stat = $("input:radio[name=scenario1Options]:checked").val();
-    playerCharStats.pushStat(stat)
     $("#agilDisplay").html(playerCharStats.agility);
     $("#strDisplay").html(playerCharStats.strength);
     $("#charDisplay").html(playerCharStats.charisma);
     $("#intDisplay").html(playerCharStats.intelligence);
-
-
   });
-  $("#scenario1Complete").click(function(){
-    $("#scenario1Outcome").slideUp(500);
+  $(".scenario1Complete").click(function(){
+    $(".outcome").slideUp(500);
     $("#scenario2").fadeIn(500).delay(500);
-
   });
+
   $("#scenario2Form").submit(function(event){
     event.preventDefault();
     $("#scenario2").slideUp(500);
@@ -41,13 +30,12 @@ $(function(){
     $("#strDisplay").html(playerCharStats.strength);
     $("#charDisplay").html(playerCharStats.charisma);
     $("#intDisplay").html(playerCharStats.intelligence);;
-
   });
-  $("#scenario2Complete").click(function(){
-    $("#scenario2Outcome").slideUp(500);
+  $(".scenario2Complete").click(function(){
+    $(".outcome").slideUp(500);
     $("#scenario3").fadeIn(500).delay(500);
-
   });
+
   $("#scenario3Form").submit(function(event){
     event.preventDefault();
     $("#scenario3").slideUp(500);
@@ -60,10 +48,11 @@ $(function(){
     $("#intDisplay").html(playerCharStats.intelligence);;
 
   });
-  $("#scenario3Complete").click(function(){
-    $("#scenario3Outcome").slideUp(500);
+  $(".scenario3Complete").click(function(){
+    $(".outcome").slideUp(500);
     $("#scenario4").fadeIn(500).delay(500);
   });
+
   $("#scenario4Form").submit(function(event){
     event.preventDefault();
     $("#scenario4").slideUp(500);
@@ -74,10 +63,9 @@ $(function(){
     $("#strDisplay").html(playerCharStats.strength);
     $("#charDisplay").html(playerCharStats.charisma);
     $("#intDisplay").html(playerCharStats.intelligence);;
-
   });
-  $("#scenario4Complete").click(function(){
-    $("#scenario4Outcome").slideUp(500);
+  $(".scenario4Complete").click(function(){
+    $(".outcome").slideUp(500);
     $("#results").fadeIn(500).delay(500);
   });
 });
