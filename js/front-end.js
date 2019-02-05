164c8da3e0becb98b09dbce0c9a85ee135ddec57
Nathan Nielson and Slater Smith <slater@>
2019-02-04 16:37:41 -0800

    add fixing from merging

--- js/front-end.js
+++ js/front-end.js
@@ -6,19 +6,15 @@ $(function(){
 
   $("#scenario1Form").submit(function(event){
     event.preventDefault();
-<<<<<<< HEAD
-    $("#scenario1").hide();
+    $("#scenario1").slideUp(500);
     var stat = $("input:radio[name=scenario1Options]:checked").val();
-    $("#scenario1Outcome-"+stat).show();
     playerCharStats.pushStatFirst(stat)
+    $("#scenario1Outcome-"+stat).fadeIn(500).delay(500);
     console.log(playerCharStats);
 
 
   });
   $(".scenario1Complete").click(function(){
-    $(".outcome").hide();
-    $("#scenario2").show();
-=======
     $("#scenario1").slideUp(500);
     $("#scenario1Outcome").fadeIn(500).delay(500);
     var stat = $("input:radio[name=scenario1Options]:checked").val();
@@ -33,91 +29,55 @@ $(function(){
   $("#scenario1Complete").click(function(){
     $("#scenario1Outcome").slideUp(500);
     $("#scenario2").fadeIn(500).delay(500);
->>>>>>> master
 
   });
   $("#scenario2Form").submit(function(event){
     event.preventDefault();
-<<<<<<< HEAD
-    $("#scenario2").hide();
-=======
     $("#scenario2").slideUp(500);
-    $("#scenario2Outcome").fadeIn(500).delay(500);
->>>>>>> master
     var stat = $("input:radio[name=scenario2Options]:checked").val();
-    $("#scenario2Outcome-"+stat).show();
     playerCharStats.pushStat(stat)
+    $("#scenario2Outcome-"+stat).fadeIn(500).delay(500);
     $("#agilDisplay").html(playerCharStats.agility);
     $("#strDisplay").html(playerCharStats.strength);
     $("#charDisplay").html(playerCharStats.charisma);
     $("#intDisplay").html(playerCharStats.intelligence);;
 
   });
-<<<<<<< HEAD
-  $(".scenario2Complete").click(function(){
-    $(".outcome").hide();
-    $("#scenario3").show();
-=======
   $("#scenario2Complete").click(function(){
     $("#scenario2Outcome").slideUp(500);
     $("#scenario3").fadeIn(500).delay(500);
->>>>>>> master
 
   });
   $("#scenario3Form").submit(function(event){
     event.preventDefault();
-<<<<<<< HEAD
-    $("#scenario3").hide();
-=======
     $("#scenario3").slideUp(500);
-    $("#scenario3Outcome").fadeIn(500).delay(500);
->>>>>>> master
     var stat = $("input:radio[name=scenario3Options]:checked").val();
-    $("#scenario3Outcome-"+stat).show();
     playerCharStats.pushStat(stat)
+    $("#scenario3Outcome-"+stat).fadeIn(500).delay(500);
     $("#agilDisplay").html(playerCharStats.agility);
     $("#strDisplay").html(playerCharStats.strength);
     $("#charDisplay").html(playerCharStats.charisma);
     $("#intDisplay").html(playerCharStats.intelligence);;
 
   });
-<<<<<<< HEAD
-  $(".scenario3Complete").click(function(){
-    $(".outcome").hide();
-    $("#scenario4").show();
-=======
   $("#scenario3Complete").click(function(){
     $("#scenario3Outcome").slideUp(500);
     $("#scenario4").fadeIn(500).delay(500);
->>>>>>> master
-
   });
   $("#scenario4Form").submit(function(event){
     event.preventDefault();
-<<<<<<< HEAD
-    $("#scenario4").hide();
-=======
     $("#scenario4").slideUp(500);
-    $("#scenario4Outcome").fadeIn(500).delay(500);
->>>>>>> master
     var stat = $("input:radio[name=scenario4Options]:checked").val();
-    $("#scenario4Outcome-"+stat).show();
     playerCharStats.pushStat(stat)
+    $("#scenario4Outcome-"+stat).fadeIn(500).delay(500);
     $("#agilDisplay").html(playerCharStats.agility);
     $("#strDisplay").html(playerCharStats.strength);
     $("#charDisplay").html(playerCharStats.charisma);
     $("#intDisplay").html(playerCharStats.intelligence);;
 
   });
-<<<<<<< HEAD
-  $(".scenario4Complete").click(function(){
-    var stat = playerCharStats.decideEnd()
-    console.log(stat);
-    $(stat).show();
-=======
   $("#scenario4Complete").click(function(){
     $("#scenario4Outcome").slideUp(500);
     $("#results").fadeIn(500).delay(500);
->>>>>>> master
   });
 });
